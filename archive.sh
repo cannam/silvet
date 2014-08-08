fc497ba9c8f01bd6829f3edd79223a5f1b3e5bb2
Chris Cannam <c.cannam@qmul.ac.uk>
2014-08-08 11:50:52 +0100

    Add file to archive from latest tag

---
+++ archive.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+tag=$(hg tags | grep -v ^tip | head -1 | awk '{ print $1 }')
+out="/tmp/silvet-$tag.tar.bz2"
+echo "Archiving from tag $tag to file $out"
+if [ -z "$tag" ]; then 
+    echo "ERROR: No tag found!"
+    exit 1
+fi
+hg archive -r"$tag" -S --exclude notes --exclude papers --exclude mirex2012-matlab --exclude testdata --exclude yeti "$out"
